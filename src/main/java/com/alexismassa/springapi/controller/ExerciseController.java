8850579e5b19873b74c59e50cce4453af18ba7c7
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2022-09-16 11:00:49 +0200

    Added cross origin to authorize VueJS app

--- src/main/java/com/alexismassa/springapi/controller/ExerciseController.java
+++ src/main/java/com/alexismassa/springapi/controller/ExerciseController.java
@@ -3,6 +3,7 @@ package com.alexismassa.springapi.controller;
 import com.alexismassa.springapi.entity.Exercise;
 import com.alexismassa.springapi.repository.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:8081/")
 public class ExerciseController {
 
     @Autowired
